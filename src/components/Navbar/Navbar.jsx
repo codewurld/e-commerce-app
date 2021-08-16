f36720f9ee25d33fe302c1b133e3b6a1619a4c82
codewurld <sholaq@gmail.com>
2021-08-16 17:50:18 +0100

    mock items added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from "@material-ui/core";
+import { ShoppingCart } from '@material-ui/icons';
+import logo from '../../assets/NOMADS_MASK.jpg'
+import useStyles from './styles' // use styles from styles.js
+
+
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="NOMADS. " height="25px" className={classes.image} />
+                        NOMADS.
+                    </Typography>
+                    <div className={classes.grow}>
+                        <div className={classes.button}><IconButton aria-label="Show cart items" color="inherit">
+                            {/* Display list of returned items */}
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+
+
+
+                        </IconButton>
+
+                        </div>
+
+                    </div>
+                </Toolbar>
+
+            </AppBar>
+        </div>
+    );
+}
+
+export default Navbar;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from '../Product/Product';
+// import ukHoodie from '../assets/nomadscapblack.png'
+
 
 
 const products = [
@@ -9,21 +11,21 @@ const products = [
         name: "NOMADS. Hoodie UK",
         description: "Black Hoodie UK Edition",
         price: "£45",
-        image: "https://images.unsplash.com/photo-1564557287817-3785e38ec1f5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG9vZGllfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60"
+        image: "https://images.unsplash.com/photo-1564557287817-3785e38ec1f5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG9vZGllfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60",
     },
     {
         id: 2,
         name: "NOMADS. Baseball Cap",
         description: "Flat baseball cap",
         price: "£27",
-        image: "../assets/nomadscapblack.png"
+        image: "https://images.unsplash.com/photo-1556306535-0f09a537f0a3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FwfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60",
     },
     {
         id: 3,
         name: "NOMADS. Face Mask",
         description: "Black face mask",
         price: "£8",
-        image: "../assets/NOMADS_MASK.jpg"
+        image: "https://images.unsplash.com/photo-1621609764049-5ee1db3d7c35?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFncGFja3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60",
     }
 ]
 
