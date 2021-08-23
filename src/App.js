f833a1f24509b12d8edff8b4c3daaa00a7db10f4
codewurld <sholaq@gmail.com>
2021-08-23 12:17:36 +0100

    dynamic cart icon update

--- src/App.js
+++ src/App.js
@@ -4,26 +4,35 @@ import { Products, Navbar } from './components';
 
 
 const App = () => {
+    // useState for later use when product is fetched
 
     const [products, setProducts] = useState([]);
+
+    // cart is empty, when item is added, cart updates
     const [cart, setCart] = useState({});
 
+    // fetch product from commerce js DB
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
 
         setProducts(data);
     }
 
+    // function to retrieve items user places in cart
+
     const fetchCart = async () => {
-        setCart(await commerce.cart.retrieve())
+        setCart(await commerce.cart.retrieve());
     }
 
+    // async function to add item to cart when cart icon is clicked
+    // function should take two items - the id of the product and quantity selected
     const handleAddToCart = async (productId, quantity) => {
         const item = await commerce.cart.add(productId, quantity);
 
         setCart(item.cart);
     };
 
+    // can use multiple effects in one useEffect
     useEffect(() => {
         fetchProducts();
         fetchCart();
@@ -33,7 +42,7 @@ const App = () => {
 
     return (
         <div>
-            <Navbar />
+            <Navbar totalItemsSelected={cart.total_items} />
             <Products products={products} onAddToCart={handleAddToCart} />
         </div>
     );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,9 @@ import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/NOMADS_MASK.jpg'
 import useStyles from './styles' // use styles from styles.js
 
+// pass prop from App to dynamically change number of items displayed in cart
 
-const Navbar = () => {
+const Navbar = ({ totalItemsSelected }) => {
     const classes = useStyles();
 
     return (
@@ -13,13 +14,13 @@ const Navbar = () => {
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography variant="h6" className={classes.title} color="inherit">
-                        {/* <img src={logo} alt="NOMADS. " height="25px" className={classes.image} /> */}
+                        <img src={logo} alt="NOMADS. " height="25px" className={classes.image} />
                         NOMADS.
                     </Typography>
                     <div className={classes.grow}>
                         <div className={classes.button}><IconButton aria-label="Show cart items" color="inherit">
                             {/* Display list of returned items */}
-                            <Badge badgeContent={2} color="secondary">
+                            <Badge badgeContent={totalItemsSelected} color="secondary">
                                 <ShoppingCart />
                             </Badge>
 
